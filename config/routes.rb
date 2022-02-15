299c471d1497cda10f587da347df161719b8a71e
Andriy1328 <ankle1328@gmail.com>
2022-02-15 11:47:37 +0200

    Change root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  get 'welcome/index'
+  
 
   resources :docs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do 
+    root "docs#index", as: "authenticated_root"
+  end
+  root 'welcome#index'
 end
